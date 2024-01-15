7e25550c8174611db1e15fc84399f20e2572ea0c
mrmayank11 <mrmayankrawat00@gmail.com>
2024-01-15 16:43:13 +0530

    completed 2nd stage

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController: ControllerBase
+{
+    private readonly DataContext context;
+
+    public UsersController( DataContext context){
+        this.context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
+        var users=await context.Users.ToListAsync();
+
+        return users;
+    }
+
+    [HttpGet("{id}")]
+
+    public async Task<ActionResult<AppUser>> GetUser(int id){
+        var user=await context.Users.FindAsync(id);
+
+        return user;
+    }
+}
